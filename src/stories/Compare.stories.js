95ec03b489305abb637bde0ed3a2d705bda8d087
saritChauhan <saritsinghchauhan@gmail.com>
2022-02-17 11:59:49 +0530

    test data removed from npm package

--- src/stories/Compare.stories.js
+++ src/stories/Compare.stories.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { storiesOf } from "@storybook/react"
 import { CompareData } from "../components/compare/Compare";
-import {metadata} from '../utils/metadata' 
-import {sampledata} from '../utils/sampledata'
+import {metadata} from '../../testData/metadata'
+import {sampledata} from '../../testData/sampledata'
 
 const stories = storiesOf('App test', module);
 
